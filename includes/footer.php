490a318f4f6666192a7977f34c084a61d509a3c9
Houler57 <83579721+Houler57@users.noreply.github.com>
2021-05-05 20:05:52 +0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<!--Grid column-->
 		<div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-		<h5 class="text-uppercase">The Kickbackers</h5>
+		<h5 class="text-white">The Kickbackers</h5>
 
 		<p>
 			The Kickbackers was founded in 2021
@@ -15,20 +15,20 @@
 
 		<!--Grid column-->
 		<div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-		<h5 class="text-uppercase">Get Around</h5>
+		<h5 class="text-white">Get Around</h5>
 
 		<ul class="list-unstyled mb-0">
 			<li>
-			<a href="aboutus.php" class="text">About Us</a>
+			<a href="aboutus.php" class="text-white">About Us</a>
 			</li>
 			<li>
-			<a href="productlisting.php" class="text">Product</a>
+			<a href="productlisting.php" class="text-white">Product</a>
 			</li>
 			<li>
-			<a href="contactus.php" class="text">Contact Us</a>
+			<a href="contactus.php" class="text-white">Contact Us</a>
 			</li>
 			<li>
-			<a href="#!" class="text">Gallery</a>
+			<a href="#!" class="text-white">Gallery</a>
 			</li>
 		</ul>
 		</div>
@@ -62,7 +62,7 @@
 	<!-- Copyright -->
 	<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
 	© 2020 Copyright:
-	<a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+	<a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
 	</div>
 </div>
 <!-- Copyright -->

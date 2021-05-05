95f82be6dfac7e5e93b8d64e2002cf43d54f6e31
Houler57 <83579721+Houler57@users.noreply.github.com>
2021-05-05 20:41:35 +0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -37,16 +37,16 @@
 		<h5 class="text-white">We Accept</h5>
 		<ul class="list-unstyled mb-0">
 			<li>
-			<a href="aboutus.php" class="text-white">About Us</a>
+			<a class="visa"><img src="Visa.png" alt="Visa" width="6" height="5"></a>
 			</li>
 			<li>
-			<a href="productlisting.php" class="text-white">Product</a>
+			<a class="master"><img src="mastercard.png" alt="mastercard" width="6" height="2"></a>
 			</li>
 			<li>
-			<a href="contactus.php" class="text-white">Contact Us</a>
+			<a class="paypal"><img src="paypal.png" alt="paypal" width="6" height="2"></a>
 			</li>
 			<li>
-			<a href="#!" class="text-white">Gallery</a>
+			<a class="american_express"><img src="American_Express.png" alt="American_Express" width="6" height="4"></a>
 			</li>
 		</ul>
 		</div>

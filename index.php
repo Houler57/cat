6bcb7f3f9fa54e3d3d6fc5fb614b041ca78f8a55
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-05-05 22:47:09 +1000

    updated index.php

--- index.php
+++ index.php
@@ -4,12 +4,29 @@
 <?php
 include("./includes/header.php");
 ?>
+<title>Home Page</title>
+	<div class="jumbotron text-center">
+		<h1><small>Shine </small>Smile</h1>
+		<p><i>Shine Smile: Tiered & Bundled Oral Hygiene Solutions</i></p>
+	</div>
 </head>
 
 <body>
 <?php
 include("./includes/navbar.php");
 ?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-6 col-sm-4">
+			<h2>Welcome to Shine Smile!</h2>
+		</div>
+<!--Code Line Break!-->
+		<img src="./image/Shinesmile logo.png" alt="Shine Smile Logo in PNG" class="img-fluid">
+<!--Double Code LINE BREAK!!-->
+	</div>
+</div>
+<!--T R I P L E Code Line Break!-->
+<!--QUADRUPLE Code ... LINE BREAK!!!-->
 </body>
 
 <footer>

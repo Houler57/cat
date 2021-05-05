bb5054acdac55721522dcace27659c3ca6ad47bf
s3850910 <71703284+03850910@users.noreply.github.com>
2021-05-05 22:12:57 +1000

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <ul class="navbar-nav">
         <li class="nav-item active">
-            <a class="nav-link" href="index.jsp">Home</a>
+            <a class="nav-link" href="index.php">Home</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Link</a>

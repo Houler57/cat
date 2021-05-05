fcff0ba312a264ab3eb2fe446dce0f45374cd9cb
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-05-05 21:33:03 +1000

    changed the image values to test

--- productlisting.php
+++ productlisting.php
@@ -19,19 +19,19 @@ include("./includes/header.php");
 	<div class="row row-no-gutters">
 		<div class="clearfix visible-xs-block">
 			<div class="col-md-6">
-			<img src="" alt="" class="img-thumbnail">
+			<img src="./image/tier1.png" alt="" class="img-thumbnail">
 				<h3>Tier 1</h3>
 				<p class="text-muted">Our flagship and most suitable tiered bundle</p>
 				<button type="button" class="btn btn-default">Choose Bundle 1</button>
 			</div>
 			<div class="col-md-6">
-			<img src="" alt="" class="img-thumbnail">
+			<img src="./image/tier2.png" alt="" class="img-thumbnail">
 				<h3>Tier 2</h3>
 				<p class="text-muted">Tier 2 is most suitable for a small family</p>
 				<button type="button" class="btn btn-default">Choose Bundle 2</button>
 			</div>
 			<div class="col-md-6">
-			<img src="" alt="" class="img-thumbnail">
+			<img src="./image/tier3.png" alt="" class="img-thumbnail">
 				<h3>Tier 3</h3>
 				<p class="text-muted">This tier is the most cost-effective choice for large families</p>
 				<button type="button" class="btn btn-default">Choose Bundle 3</button>

f77f1ba99ec3e2b370f96bd4bfa38296434b7563
s3665479 <83631220+s3665479@users.noreply.github.com>
2021-05-05 22:50:22 +1000

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -6,15 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="aboutus.css">
-    <?php
-   // include 'filename';  header here 
-   // include 'filename';  footer here
-    ?>
+
+<?php
+include("./includes/header.php");
+?>
 </head>
 <body>
 <div class="shine">
+<?php
+include("./includes/navbar.php");
+?>
             <h1>About Shine Smile</h1>
-            <h5>We provide our customers with the best dental hydine products </h5>
+            <h5>We Provide Our Customers With The Best Dental Hygiene Products </h5>
           </div>
           <table>
             <tr>
@@ -22,24 +25,30 @@
               <th><img src="image/Toothbrush with logo.png"></th>
             </tr>
             <tr>
-              <th>What we do</th>
-              <th>Why shine smile</th>
+              <th>What We Do</th>
+              <th>Why Shine Smile</th>
             </tr>
             <tr>
-              <td>Put word here
-                <br/>Put word here
-                <br/>Put word here
-                <br/>Put word here
-                <br/>Put word here
+              <td>
+                <br/>We Provide a tiered bundle of services made up of dental hygiene products
+                <br/>which are cheaper than buying the products individually in commercial stores
+                <br/>While at the same time, YOU are saving the environment!
+                <br/>
               </td>
-              <td>Put word here
-                <br/>Put word here
-                <br/>Put word here
-                <br/>Put word here
-                <br/>Put word here
+              <td>
+                <br/>Our competitors do not offer tiered pricing of bundled products
+                <br/>they only sell bamboo toothbrushes from the same manufacturer
+                <br/>With Shine Smile, we also allow the customer to donate. 
+                <br/>
               </td>
             </tr>
           </table>
           </div>
 </body>
+
+<footer>
+<?php
+include("./includes/footer.php");
+?>
+</footer>
 </html>

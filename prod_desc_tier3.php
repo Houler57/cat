3fd4a0c56d6e11979c83bb71916feedc70869460
Houler57 <83579721+Houler57@users.noreply.github.com>
2021-05-05 19:09:12 +0700

    Add files via upload

---
+++ prod_desc_tier3.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Product Description</title>
+<?php
+include("./includes/header.php");
+include("./includes/navbar.php");
+?>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+	<h1><small>Product </small>Description</h1>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="prod_dec_tier3">
+			<img src="./image/tier3.jpg" alt="" class="tier3">
+				<h3>Tier 3</h3>
+				<p class="text-muted">In this Tier 3, you will get 5 of our ShineSmile toothbrush, 2 medium size ShineSmile toothpaste, and 1 ShineSmile mouthwash.</p>
+				<h5>Price :</h5>
+				<p class="text-muted">$50</p>
+				<button type="button" class="add_to_cart">Add to Cart</button>
+			</div>
+	</div>
+</div>
+	
+</body>
+
+<footer>
+<?php
+include("./includes/footer.php");
+?>
+</footer>
+</html>

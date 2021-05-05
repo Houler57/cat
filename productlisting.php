1b2de743047ebdd2d4f0ae9406bdfaa55c13ebe2
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-05-05 21:36:18 +1000

    updated productlisting.php

--- productlisting.php
+++ productlisting.php
@@ -17,19 +17,19 @@ include("./includes/header.php");
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-6">
+		<div class="col-sm-6">
 			<img src="./image/tier1.png" alt="" class="img-thumbnail">
 				<h3>Tier 1</h3>
 				<p class="text-muted">Our flagship and most suitable tiered bundle</p>
 				<button type="button" class="btn btn-default">Choose Bundle 1</button>
 			</div>
-			<div class="col-md-6">
+			<div class="col-sm-6">
 			<img src="./image/tier2.png" alt="" class="img-thumbnail">
 				<h3>Tier 2</h3>
 				<p class="text-muted">Tier 2 is most suitable for a small family</p>
 				<button type="button" class="btn btn-default">Choose Bundle 2</button>
 			</div>
-			<div class="col-md-6">
+			<div class="col-sm-6">
 			<img src="./image/tier3.png" alt="" class="img-thumbnail">
 				<h3>Tier 3</h3>
 				<p class="text-muted">This tier is the most cost-effective choice for large families</p>

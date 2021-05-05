1b538ad1e17dcb17e515a85e4e31ae0685cc573d
pastamanwjdgwdexit <S3850910@student.rmit.edu.au>
2021-05-06 01:44:11 +1000

    css linked

--- aboutus.php
+++ aboutus.php
@@ -14,41 +14,38 @@ include("./includes/header.php");
 </head>
 
 <body>
-    <div class="shine">
-        <?php
-include("./includes/navbar.php");
-?>
-
-        <main>
+    <?php  include("./includes/navbar.php");?>
+    <main>
+        <div class="shine">
             <h1>About Shine Smile</h1>
             <h5>We Provide Our Customers With The Best Dental Hygiene Products </h5>
-    </div>
-    <table>
-        <tr>
-            <th><img src="image/Toothbrush with logo with ingraving.png"></th>
-            <th><img src="image/Toothbrush with logo.png"></th>
-        </tr>
-        <tr>
-            <th>What We Do</th>
-            <th>Why Shine Smile</th>
-        </tr>
-        <tr>
-            <td>
-                <br />We Provide a tiered bundle of services made up of dental hygiene products
-                <br />which are cheaper than buying the products individually in commercial stores
-                <br />While at the same time, YOU are saving the environment!
-                <br />
-            </td>
-            <td>
-                <br />Our competitors do not offer tiered pricing of bundled products
-                <br />they only sell bamboo toothbrushes from the same manufacturer
-                <br />With Shine Smile, we also allow the customer to donate.
-                <br />
-            </td>
-        </tr>
-    </table>
-    </div>
-  </main>
+        </div>
+        <table>
+            <tr>
+                <th><img src="image/Toothbrush with logo with ingraving.png"></th>
+                <th><img src="image/Toothbrush with logo.png"></th>
+            </tr>
+            <tr>
+                <th>What We Do</th>
+                <th>Why Shine Smile</th>
+            </tr>
+            <tr>
+                <td>
+                    <br />We Provide a tiered bundle of services made up of dental hygiene products
+                    <br />which are cheaper than buying the products individually in commercial stores
+                    <br />While at the same time, YOU are saving the environment!
+                    <br />
+                </td>
+                <td>
+                    <br />Our competitors do not offer tiered pricing of bundled products
+                    <br />they only sell bamboo toothbrushes from the same manufacturer
+                    <br />With Shine Smile, we also allow the customer to donate.
+                    <br />
+                </td>
+            </tr>
+        </table>
+        </div>
+    </main>
 </body>
 
 <footer>
--- includes/header.php
+++ includes/header.php
@@ -5,3 +5,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 <!-- font awesome css -->
 <script src="https://kit.fontawesome.com/e4b7cacf78.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/main.css">

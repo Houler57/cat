99facd169cca5508727292e0dcc3e2990475e533
s3665479 <83631220+s3665479@users.noreply.github.com>
2021-05-05 19:15:26 +1000

    page about us

--- aboutus.php
+++ aboutus.php
@@ -5,8 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="aboutus.css">
+    <?php
+   // include 'filename';  header here 
+   // include 'filename';  footer here
+    ?>
 </head>
 <body>
-    
+<div class="shine">
+            <h1>About Shine Smile</h1>
+            <h5>We provide our customers with the best dental hydine products </h5>
+          </div>
+          <table>
+            <tr>
+              <th><img src="image/Toothbrush with logo with ingraving.png"></th>
+              <th><img src="image/Toothbrush with logo.png"></th>
+            </tr>
+            <tr>
+              <th>What we do</th>
+              <th>Why shine smile</th>
+            </tr>
+            <tr>
+              <td>Put word here
+                <br/>Put word here
+                <br/>Put word here
+                <br/>Put word here
+                <br/>Put word here
+              </td>
+              <td>Put word here
+                <br/>Put word here
+                <br/>Put word here
+                <br/>Put word here
+                <br/>Put word here
+              </td>
+            </tr>
+          </table>
+          </div>
 </body>
 </html>

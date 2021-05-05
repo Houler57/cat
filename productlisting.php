2e4841d278a9b00bfb55977e95ed2fe9ea5ad28a
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-05-05 22:53:11 +1000

    replaced string values for the image referencing

--- productlisting.php
+++ productlisting.php
@@ -17,19 +17,19 @@ include("./includes/navbar.php");
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-sm">
-			<img src="./image/tier1.jpg" alt="Tier 1 Bundle Image" class="img-fluid">
+			<img src="./image/tier 1.jpg" alt="Tier 1 Bundle Image" class="img-thumbnail">
 				<h3>Tier 1</h3>
 				<p class="text-muted">Our flagship and most suitable tiered bundle</p>
 				<a href="prod_desc_tier1.php" type="button" class="btn btn-default">Choose Bundle 1</a>
 			</div>
 			<div class="col-sm">
-			<img src="./image/tier2.jpg" alt="Tier 2 Bundle Image" class="img-fluid">
+			<img src="./image/tier 2.jpg" alt="Tier 2 Bundle Image" class="img-thumbnail">
 				<h3>Tier 2</h3>
 				<p class="text-muted">Tier 2 is most suitable for a small family</p>
 				<a href="prod_desc_tier2.php" type="button" class="btn btn-default">Choose Bundle 2</a>
 			</div>
 			<div class="col-sm">
-			<img src="./image/tier3.jpg" alt="Tier 3 Bundle Image" class="img-fluid">
+			<img src="./image/tier 3.jpg" alt="Tier 3 Bundle Image" class="img-thumbnail">
 				<h3>Tier 3</h3>
 				<p class="text-muted">This tier is the most cost-effective choice for large families</p>
 				<a href="prod_desc_tier3.php" type="button" class="btn btn-default">Choose Bundle 3</a>

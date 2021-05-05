b039d72377304c0625631d158fda5e506bec0de4
Houler57 <83579721+Houler57@users.noreply.github.com>
2021-05-05 20:36:12 +0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -35,7 +35,7 @@
 		<!--Grid column-->
 		<div class="col-lg-3 col-md-6 mb-4 mb-md-0">
 		<h5 class="text-white">We Accept</h5>
-
+		</div>
 			<a><img src="Visa.png" alt="Visa" width="6" height="5"></a>
 			<br>
 			<a><img src="mastercard.png" alt="mastercard" width="6" height="2"></a>
@@ -43,7 +43,6 @@
 			<a><img src="paypal.png" alt="paypal" width="6" height="2"></a>
 			<br>
 			<a><img src="American_Express.png" alt="American_Express" width="6" height="4"></a>
-		</div>
 		<!--Grid column-->
 
 	</div>

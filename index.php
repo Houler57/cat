a8cf87bd10d4cf1a470100d57fc16818f3afd1a5
pastamanwjdgwdexit <S3850910@student.rmit.edu.au>
2021-05-06 18:40:45 +1000

    cdns

--- index.php
+++ index.php
@@ -14,14 +14,27 @@ include("./includes/navbar.php");
 ?>
     <main>
         <section>
-            <div class="bg-secondary pt-5 pb-5" style=" width: 50%; height: 50%;  ">
-                <div id="demo" class="carousel slide" data-ride="carousel">
+            <div id="message-container" class="bg-secondary pt-5 pb-5">
+
+                <div class="m-5 " id="homemessage" style=" width: 30%; max-height:100%;">
+
+                    <h1>
+                        Get your complete dental care package delivered to your door
+                    </h1>
+
+
+                    <a style="margin-top: 40%;" name="" id="" class="btn btn-danger" href="#"
+                        role="button">Subscribe</a>
+                </div>
+
+                <div id="demo" class="m-5 pull-right carousel slide" data-ride="carousel"
+                    style=" width: 30%; height: 30%;  ">
 
                     <!-- Indicators -->
                     <ul class="carousel-indicators">
-                        <li data-target="#demo" data-slide-to="0" class="bg-danger active"></li>
-                        <li data-target="#demo" data-slide-to="1"></li>
-                        <li data-target="#demo" data-slide-to="2"></li>
+                        <li data-target="#demo" data-slide-to="0" class="bg-secondary active"></li>
+                        <li data-target="#demo" data-slide-to="1" class="bg-secondary"></li>
+                        <li data-target="#demo" data-slide-to="2" class="bg-secondary"></li>
                     </ul>
 
                     <!-- The slideshow -->
@@ -39,19 +52,41 @@ include("./includes/navbar.php");
 
                     <!-- Left and right controls -->
                     <a class=" carousel-control-prev" href="#demo" data-slide="prev">
-                        <span class="bg-danger carousel-control-prev-icon"></span>
+                        <span class=" carousel-control-prev-icon"></span>
                     </a>
                     <a class=" carousel-control-next" href="#demo" data-slide="next">
-                        <span class="bg-danger carousel-control-next-icon"></span>
+                        <span class=" carousel-control-next-icon"></span>
                     </a>
                 </div>
             </div>
         </section>
 
-        <section>
+        <div id="home-description">
 
+            <div style="width: 70%;" class="p-5">
+                <h1 class="mb-5">What Is Shine Smile dental care Subscription Box?</h1>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate soluta, magni voluptates ut quo
+                    officia eos eveniet aspernatur voluptas? Obcaecati nulla iusto architecto unde quo ipsa, provident
+                    asperiores qui soluta sit quod minima accusantium aspernatur ea! Ullam numquam optio tenetur ducimus
+                    culpa qui eligendi quidem commodi repudiandae eaque rem mollitia odit deserunt atque laboriosam
+                    facilis
+                    nostrum sunt, vero voluptatum laudantium illo maxime similique! Aspernatur, accusantium ad magni
+                    debitis
+                    sapiente possimus et aperiam rerum aliquid in neque quos illo accusamus sint excepturi harum omnis
+                    voluptates molestias eveniet quo architecto, perferendis deserunt? Ut aliquam laboriosam, earum,
+                    voluptate suscipit sed magnam rerum voluptatum facilis assumenda obcaecati facere soluta! Itaque ad
+                    illum error, laborum voluptas, explicabo vero sed cupiditate animi laboriosam amet officia qui
+                    architecto! Ducimus numquam eligendi repellat laboriosam maiores debitis corporis possimus autem,
+                    asperiores quos et fugit, provident culpa quaerat facilis unde? Commodi distinctio, ullam, maxime,
+                    qui
+                    reiciendis molestiae aspernatur quasi eius impedit inventore nam iure voluptas? Blanditiis maiores
+                    voluptatum vel facere perferendis dolor odio iste. Facilis repudiandae, omnis porro laboriosam
+                    libero
+                    nam ipsum soluta impedit quis ratione architecto perspiciatis, dignissimos vero animi.</p>
 
-        </section>
+            </div>
+        </div>
     </main>
 </body>
 

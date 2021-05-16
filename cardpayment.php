5deeee839a76b57e19a979e605921e284b1685e3
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-05-16 21:31:12 +1000

    updated cardpayment.php

--- cardpayment.php
+++ cardpayment.php
@@ -21,24 +21,24 @@
 	}
 	?>
 	<div class="col-md-6">
-	Email Address: <input class="form-control" type="email" name="email-address" id="email-address"><br>
+		<label for="email-address">Email Address:</label> <input class="form-control" type="email" name="email-address" id="email-address" required><br>
 	</div>
 
 	<div class="col-md-6">
-	<label for="credit-card-fullname">Name (on card):</label> <input class="form-control" type="text" name="credit-card-fullname" id="credit-card-fullname"><br>
-	<label for="credit-card-number">Credit Card (Number):</label> <input class="form-control" type="text" name="credit-card-number" id="credit-card-number"><br>
+		<label for="credit-card-fullname">Name (on card):</label> <input class="form-control" type="text" name="credit-card-fullname" id="credit-card-fullname" required><br>
+		<label for="credit-card-number">Credit Card (Number):</label> <input class="form-control" type="text" name="credit-card-number" id="credit-card-number" required><br>
 
-	<label for="credit-card-cvv">Card Verification Value (CVV):</label> <input class="form-control" type="text" name="credit-card-cvv" id="credit-card-cvv"><br>
-	<label for="credit-card-date">Date of Expiration:</label> <input class="form-control" type="date" name="credit-card-date" id="credit-card-date"><br>
+		<label for="credit-card-cvv">Card Verification Value (CVV):</label> <input class="form-control" type="text" name="credit-card-cvv" id="credit-card-cvv" required><br>
+		<label for="credit-card-date">Date of Expiration:</label> <input class="form-control" type="date" name="credit-card-date" id="credit-card-date" required><br>
 	</div>
 
 	<div class="col-md-4">
-	<label for="post-delivery-address">Address:</label> <input class="form-control" type="text" name="post-delivery-address" id="post-delivery-address">
-	<label for="suburb-of-occupation">Suburb:</label> <input class="form-control" type="text" name="suburb-of-occupation" id="suburb-of-occupation">
+		<label for="post-delivery-address">Address:</label> <input class="form-control" type="text" name="post-delivery-address" id="post-delivery-address" required>
+		<label for="suburb-of-occupation">Suburb:</label> <input class="form-control" type="text" name="suburb-of-occupation" id="suburb-of-occupation" required>
 
-	<label for="state-of-origin">State:</label> <input class="form-control" type="text" name="state-of-origin" id="state-of-origin">
-	<label for="region-postcode">Postcode:</label> <input class="form-control" type="text" name="region-postcode" id="region-postcode">
-	<label for="country-of-occupation">Country:</label> <input class="form-control" type="text" name="country-of-occupation" id="country-of-occupation">
+		<label for="state-of-origin">State:</label> <input class="form-control" type="text" name="state-of-origin" id="state-of-origin" required>
+		<label for="region-postcode">Postcode:</label> <input class="form-control" type="text" name="region-postcode" id="region-postcode" required>
+		<label for="country-of-occupation">Country:</label> <input class="form-control" type="text" name="country-of-occupation" id="country-of-occupation" required>
 	</div>
 	
 	<button class="btn btn-primary" type="submit">Submit</button>

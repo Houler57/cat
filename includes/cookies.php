5943d7211890ea0209cc988266f8b55630ae4163
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-05-17 21:35:22 +1000

    created cookies and sessions files, need to improve it for shoppingcart.php and cardpayment.php

---
+++ includes/cookies.php
@@ -0,0 +1,19 @@
+<?php // NOTE(Daniel): Values may change based on fellow team member's conventions.
+$jsonShoppingCart = fopen("/data/json/shoppingcart.json", "r") or die();
+$visitorCartJsonVals = json_decode($jsonShoppingCart); // Open then decode json vile to the directed file path.
+
+$currentJsonSessionValues = array($visitorCartJsonVals->visitingUser); // Get the associated key named: `visitingUser`, this is different to `$vistingUser`.
+
+$visitingUserCookie = $visitingUser;
+$visitorCartCookies = $visitorCartJsonVals;
+
+function cartSessionAsCookieCallback($visitingUserCookie, $visitorCartCookies) {
+	return setcookie($visitingUserCookie, $visitorCartCookies, time() + (86400 * 30), "/data/cookies"); // Sets cookie values in "data/cookies/" with a day's worth expiry.
+}
+
+if (!isset($_COOKIE[$visitingUserCookie])) {
+	$errVisitingUserCookie = "Visiting User Cookie has not been set!";
+} else {
+	return cartSessionAsCookieCallback($visitingUserCookie, $visitorCartCookies); // We used a function to apply a bit of SOLID principles.
+}
+?>

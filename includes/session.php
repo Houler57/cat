31bb0e27d3a284d3589322de83d1ce3ca0e481f8
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-05-19 14:17:42 +1000

    reworked my files for the session management and functionality. still need to know what this paths out totho

--- includes/session.php
+++ includes/session.php
@@ -1,5 +1,32 @@
 <?php
-session_start();
 
-$_SESSION[$visitingUserSess] = $visitingUser;
+function okSession($visitingUserSessionId = session_id()) {
+	return $visitingUserSessionId;
+} okSession();
+
+function okTransSid($sessionForSid = $_SESSION["session.use_trans_sid"]) {
+	return $sessionForSid;
+} okTransSid($sessionForSid);
+
+function okCookie($sessionForCookie = $_SESSION["session.use_only_cookies"]) {
+	return $sessionForCookie;
+} /* okCookie($sessionForCookie); */
+
+function setSessionCookie() {
+
+	$cookiesession = okCookie();
+
+	if (isset($_COOKIE[$cookiesession])) {
+		ini_set("session.use_trans_sid", false);
+		session_start();
+		okTransSid(1);
+		okCookie(0);
+	} else {
+		ini_set("session.use_trans_sid", true);
+		session_start();
+		okTransSid(0);
+		okCookie(1);
+	}
+} setSessionCookie();
+// "php session.use_trans_sid". [online] Available at: https://stackoverflow.com/a/10811392
 ?>
---
+++ php/post/loginredirect.php
@@ -0,0 +1,27 @@
+<?php
+$rootDir = $_SERVER["HTTP_HOST"];
+$filepath = rtrim(dirname($_SERVER["PHP_SELF"]));
+include("../jsonformats.php");
+include("../usersessiondao.php");
+include("$rootDir/includes/session.php");
+?>
+
+<?php
+
+function redirectLogin($filepath, $statusCode = 303) {
+	$rootDir = $_SERVER["HTTP_HOST"];
+	$filepath = rtrim(dirname($_SERVER["PHP_SELF"]));
+	$landingPage = 'index.php';
+	header("Location: http://$rootDir$filepath/$landingPage", true, $statusCode);
+	exit;
+} // "How do I make a redirect in PHP?". [online] Available at: https://stackoverflow.com/a/768472
+
+?>
+
+<?php
+session_start();
+session_id();
+
+loginUser($postFormParams);
+redirectLogin($filepath, 303);
+?>

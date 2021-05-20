82d1683d4d4c5ce85280dfe3e7acb16c3504df06
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-05-20 16:21:46 +1000

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -12,56 +12,44 @@
 </head>
 <body>
 	<?php include("./includes/navbar.php");  ?>
-<div class="bg-dark text-center text-lg-start">
-	<!-- Grid container -->
-	<div class="container-sm p-4">
-	<!--Grid row-->
-	<div class="row">
-		<!--Grid column-->
-		<div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-		<h5 class="text-white">The Kickbackers</h5>
+	
+	
+	<div class="container-xl p-4">
+		<div class="row">
+			<div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+				<div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+					<h5 class="text-xl-left font-weight-bold">Get Around</h5>
 
-		<p>
-			The Kickbackers was founded in 2021
-		</p>
-		</div>
-		<!--Grid column-->
+					<ul class="list-unstyled mb-0">
 
-		<!--Grid column-->
-		<div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-		<h5 class="text-white">Get Around</h5>
+					<li class="nav-item active">
+						<a class="text-xl-left nav-link" href="index.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="text-xl-left nav-link" href="./productlisting.php">Products</a>
+					</li>
+					<li class="nav-item">
+						<a class="text-xl-left nav-link" href="./aboutus.php">About Us</a>
+					</li>
+					<li class="nav-item">
+						<a class="text-xl-left nav-link" href="./contactus.php">Contact Us</a>
+					</li>
 
-		<ul class="list-unstyled mb-0">
+					<li class="nav-item">
+						<a class="text-xl-left nav-link" href="./shoppingcart.php">Cart</a>
+					</li>
 
-		<li class="nav-item active">
-            <a class=" nav-link  text-white  " href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class=" nav-link  text-white  " href="./productlisting.php">Products</a>
-        </li>
-        <li class="nav-item">
-            <a class=" nav-link  text-white  " href="./aboutus.php">About Us</a>
-        </li>
-        <li class="nav-item">
-            <a class=" nav-link  text-white  " href="./contactus.php">Contact Us</a>
-        </li>
-
-		<li class="nav-item">
-			<a class="nav-link text-white" href="./shoppingcart.php">Cart</a>
-		</li>
-
-		</ul>
-		</div>
-</body>
-	<!-- Copyright -->
-	<div class="row">
-		<div class="text-white" style="background-color: rgba(0, 0, 0, 0.2);">
-			<div class="row">
-			© 2020 Copyright:
-			<a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-<!-- Copyright -->
+
+	<div class="container-sm">
+		<img class="img-thumbnail float-left" src="./image/Doctor 2.jfif" alt="A Doctor taken in a landscape photo">
+		<img class="img-thumbnail float-right" src="./image/Docter 1.jfif" alt="There's a Doctor who is an oral specialist too">
+	</div>
+
+</body>
+
 </html>
